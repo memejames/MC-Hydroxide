8c5998c5715f977802dbbfe29543888d34d48130
Hosvile <100496294+Hosvile@users.noreply.github.com>
2023-08-26 11:19:50 +0800

    update main.lua

--- ui/main.lua
+++ ui/main.lua
@@ -22,7 +22,7 @@ getgenv().touchPoints = {}
 getgenv().touching = {}
 getgenv().conduct = 0
 getgenv().pressHold = false
-
+getgenv().mainBase = Interface.Base
 if signaluis then
 	signaluis:Disconnect()
 end
