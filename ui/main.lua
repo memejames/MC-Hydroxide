77a281ba0a599a402593c57db589da926224dbdd
Hosvile <100496294+Hosvile@users.noreply.github.com>
2023-09-10 23:55:31 +0800

    update main.lua

--- ui/main.lua
+++ ui/main.lua
@@ -81,7 +81,7 @@ function moduleError(err, module)
 	end)
 end
 
-local s1, e1, s2, e2, s3, e3, s4, e4, s5, e5, s6, e6 = pcall(function()
+s1, e1, s2, e2, s3, e3, s4, e4, s5, e5, s6, e6 = pcall(function()
 	RemoteSpy = import("ui/modules/RemoteSpy");
 end), pcall(function(err)
 	ClosureSpy = import("ui/modules/ClosureSpy");
