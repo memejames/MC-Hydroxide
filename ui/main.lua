9eba4aaedb33e8622600d906eb405555b127952f
Hosvile <100496294+Hosvile@users.noreply.github.com>
2023-09-11 00:09:51 +0800

    update main.lua

--- ui/main.lua
+++ ui/main.lua
@@ -81,27 +81,27 @@ function moduleError(err, module)
 	end)
 end
 
-s1, e1 = pcall(function()
+s1_, e1_ = pcall(function()
 	RemoteSpy = import("ui/modules/RemoteSpy");
 end)
-s2, e2 = pcall(function(err)
+s2_, e2_ = pcall(function(err)
 	ClosureSpy = import("ui/modules/ClosureSpy");
 end)
-s3, e3 = pcall(function(err)
+s3_, e3_ = pcall(function(err)
 	ScriptScanner = import("ui/modules/ScriptScanner");
 end)
-s4, e4 = pcall(function(err)
+s4_, e4_ = pcall(function(err)
 	ModuleScanner = import("ui/modules/ModuleScanner");
 end)
-s5, e5 = pcall(function(err)
+s5_, e5_ = pcall(function(err)
 	UpvalueScanner = import("ui/modules/UpvalueScanner");
 end)
-s6, e6 = pcall(function(err)
+s6_, e6_ = pcall(function(err)
 	ConstantScanner = import("ui/modules/ConstantScanner"); 
 end)
 
 for i = 1, 6 do
-	moduleResponse = loadstring(string.format("return e%d", i))()
+	moduleResponse = loadstring(string.format("return e%d_", i))()
 	if moduleResponse then
 		moduleError(moduleResponse, moduleId[i])
 	end
